7aeeb16807567b9cc68823b32b25d832b78937d7
alex501212 <alexthepatterson@hotmail.com>
2022-07-30 00:10:35 +0100

    created new updated site with react and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Carousel from "./components/Carousel/Carousel";
+import Sidebar from "./components/Sidebar/Sidebar";
+
+function App() {
+  return (
+    <div>
+      <Sidebar />
+      <div>
+        <Carousel />
+      </div>
+    </div>
+  );
+}
+
+export default App;
